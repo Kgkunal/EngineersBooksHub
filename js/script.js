5f76eb3cd2a3d22489f3791569489909e9fd8a9a
Kunal D Gavhane <kgkunalgavhane1721@gmail.com>
2024-02-19 16:49:35 +0530

    popup style login form added with close btn

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
+// search btn
 searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick=()=>{
     searchForm.classList.toggle('active');
+}
+
+// login form btn
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.add('active');
+};
+
+// login form close btn
+let closebtn = document.querySelector('#close-login-btn');
+closebtn.onclick=()=>{
+loginForm.classList.remove( 'active' );
 }
 
 window.onscroll =()=>{
